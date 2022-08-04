db6167bf14b6ef3f127e17cff976cc1a70907856
LyonSyonII <slimystudios@protonmail.com>
2022-08-04 22:02:00 +0200

    Completed native build

--- src/application.rs
+++ src/application.rs
@@ -1,12 +1,63 @@
-use std::io::Read;
+use std::{
+    io::Read,
+};
 
-use wry::{application::{self, window::{WindowBuilder}, window::Icon, event_loop::ControlFlow, event::{Event, WindowEvent}}, webview::WebViewBuilder};
+use wry::{
+    application::{
+        self,
+        event::{Event, WindowEvent},
+        event_loop::ControlFlow,
+        window::Icon,
+        window::{WindowBuilder, self}, dpi::{Size, PhysicalSize}, menu::MenuBar,
+    },
+    webview::WebViewBuilder,
+};
+
+use crate::nativefy::{def_name};
+
+const fn def_false() -> bool { false }
+const fn def_true() -> bool { true }
+pub const fn def_height() -> u32 { 800 }
+pub const fn def_width() -> u32 { 1280 }
 
 #[derive(serde::Deserialize, Debug)]
-struct AppSettings<'i> {
-    url: &'i str,
-    title: &'i str,
-    icon: bool,
+pub struct AppSettings<'i> {
+    // Creation Options
+    target_url: &'i str,
+    
+    icon: Option<&'i str>,
+
+    #[serde(default = "def_name")]
+    name: &'i str,
+
+    // Window Options
+    #[serde(default = "def_false")]
+    always_on_top: bool,
+    
+    #[serde(default = "def_false")]
+    fullscreen: bool,
+    
+    #[serde(default = "def_height")]
+    height: u32,
+    
+    #[serde(default = "def_width")]
+    width: u32,
+    
+    #[serde(default = "def_false")]
+    hide_window_frame: bool,
+    
+    #[serde(default = "def_false")]
+    show_menu_bar: bool,
+    
+    #[serde(default = "u32::max_value")]
+    max_width: u32,
+    #[serde(default = "u32::max_value")]
+    max_height: u32,
+    
+    #[serde(default = "u32::min_value")]
+    min_width: u32,
+    #[serde(default = "u32::min_value")]
+    min_height: u32,
 }
 
 pub fn run(mut file: std::fs::File) -> wry::Result<()> {
@@ -16,31 +67,45 @@ pub fn run(mut file: std::fs::File) -> wry::Result<()> {
 
     let settings: AppSettings = toml::from_str(&buffer).unwrap();
     let event_loop = application::event_loop::EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title(settings.name)
+        .with_always_on_top(settings.always_on_top)
+        .with_fullscreen(settings.fullscreen.then_some(window::Fullscreen::Borderless(None)))
+        .with_inner_size(Size::Physical(PhysicalSize::new(settings.width, settings.height)))
+        .with_max_inner_size(Size::Physical(PhysicalSize::new(settings.max_width, settings.max_height)))
+        .with_min_inner_size(Size::Physical(PhysicalSize::new(settings.min_width, settings.min_height)))
+        .with_decorations(settings.hide_window_frame)
+        .with_menu(MenuBar::new())
+        .build(&event_loop)?;
     
-    let window = WindowBuilder::new().with_title(settings.title).build(&event_loop)?;
-        
-    if settings.icon {
-        let icon = std::fs::read("icon.png").unwrap_or_exit("icon.png is not present in the root directory.\nSet icon=false on naty.toml or add an icon");
+    window.set_menu(settings.show_menu_bar.then_some(MenuBar::new()));
+    
+    if let Some(path) = settings.icon {
+        let icon = std::fs::read(path).unwrap_or_exit(format!("Icon '{path}' does not exist. Please change the 'icon' option in 'naty.toml' or remove it completely."));
 
         let icon = image::load_from_memory(&icon).unwrap();
         let width = icon.width();
         let height = icon.height();
         window.set_window_icon(Some(Icon::from_rgba(icon.into_bytes(), width, height)?));
     }
-    
+
     let _webview = WebViewBuilder::new(window)?
-        .with_url(settings.url)?
+        .with_url(settings.target_url)?
         .build()?;
-    
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
-        
-        if let Event::WindowEvent { event: WindowEvent::CloseRequested, .. } = event {
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
             *control_flow = ControlFlow::Exit
         }
     });
 }
-
 
 trait Exit<T> {
     fn unwrap_or_exit(self, msg: impl AsRef<str>) -> T;
--- src/nativefy.rs
+++ src/nativefy.rs
@@ -1,3 +1,112 @@
+use crate::application::{def_height, def_width};
+use clap::{clap_derive::ArgEnum, Parser};
+use std::path::{Path, PathBuf};
+
+pub fn def_out_dir() -> PathBuf { std::env::current_dir().unwrap() }
+pub const fn def_name() -> &'static str { "Application Title" }
+
+#[derive(Parser, Debug, serde::Serialize)]
+#[clap(author, version, about)]
+struct Cli {
+    /// Url from which the app will be created
+    #[clap()]
+    target_url: String,
+
+    /// Directory where the application will be deployed.
+    ///
+    /// If not specified, the current directory will be used.
+    #[serde(skip)]
+    #[clap(short, long, default_value = ".")]
+    output_dir: PathBuf,
+
+    /// Title of the app
+    #[clap(short, long, default_value_t = def_name().into())]
+    name: String,
+
+    #[clap(short, long, arg_enum)]
+    platforms: Vec<Platform>,
+
+    #[clap(short, long)]
+    /// Icon of the app, it must be in a ".png" format
+    icon: Option<PathBuf>,
+
+    /// Enable always on top window
+    #[clap(long)]
+    always_on_top: bool,
+    
+    /// Always start the app in full screen
+    #[clap(long)]
+    full_screen: bool,
+
+    #[clap(long, default_value_t = def_height())]
+    height: u32,
+    
+    #[clap(long, default_value_t = def_width())]
+    width: u32,
+    
+    #[clap(long)]
+    hide_window_frame: bool,
+    
+    #[clap(long)]
+    show_menu_bar: bool,
+    
+    #[clap(long, default_value_t = u32::MAX)]
+    max_width: u32,
+    #[clap(long, default_value_t = u32::MAX)]
+    max_height: u32,
+    
+    #[clap(long, default_value_t = u32::MIN)]
+    min_width: u32,
+    #[clap(long, default_value_t = u32::MIN)]
+    min_height: u32,
+
+}
+
+#[derive(Clone, Debug, PartialEq, serde::Serialize, ArgEnum)]
+enum Platform {
+    Linux,
+    Windows,
+    MacOs,
+}
+
+#[cfg(target_family = "windows")]
+pub fn copy_executable(output_dir: &Path, name: &str) -> std::io::Result<u64> {
+    let name = format!("{name}.exe");
+    std::fs::copy(std::env::current_exe()?, output_dir.join(name))
+}
+
+#[cfg(target_family = "unix")]
+pub fn copy_executable(output_dir: &Path, name: &str) -> std::io::Result<u64> {
+    std::fs::copy(std::env::current_exe()?, output_dir.join(name))
+}
+
 pub fn run() -> std::io::Result<()> {
+    let cli: Cli = Cli::parse();
+    println!("{cli:?}");
+    
+    let settings = toml::to_string_pretty(&cli).unwrap();
+    let output_dir = cli.output_dir.join(&cli.name);
+    std::fs::create_dir_all(&output_dir).expect("Could not create directory");
+    std::fs::write(output_dir.join("naty.toml"), settings).expect("Could not create naty.toml");
+    if let Some(icon) = cli.icon {
+        std::fs::copy(&icon, output_dir.join(icon.file_name().unwrap())).expect("Could not copy icon");
+    }
+    
+    if cli.platforms.is_empty() {
+        copy_executable(&output_dir, &cli.name)?;
+    } else {
+        let mut platforms = cli.platforms;
+        platforms.dedup();
+        // for platform in platforms {
+        //     match platform {
+        //         Platform::Linux => copy_linux_executable(),
+        //         Platform::Windows => copy_windows_executable(),
+        //         Platform::MacOs => copy_macos_executable(),
+        //     }
+        // }
+    }
+    
+    Ok(())
+    
     
 }
