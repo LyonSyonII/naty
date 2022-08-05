77579eb2fca19c4722fa8852ebadc6aef5c9a99a
LyonSyonII <slimystudios@protonmail.com>
2022-08-05 14:06:45 +0200

    Fixed Binary downloads

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
-cd naty_common
-cargo release $1 -x
+# cd naty_common
+# cargo release $1 -x
 
-cd ../naty_nativefy
+cd naty_nativefy
 cargo release $1 -x &
 
 cd ../naty_app
-cargo release $1 -x &
+cargo release $1 -x
 
 cd ../
 cargo release $1 -x
